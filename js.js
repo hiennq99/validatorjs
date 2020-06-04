68480d165ed2a5418b1cb93094cc60b9218465fc
rotkquanghien <hien124ytbg@gmail.com>
2020-06-04 13:18:57 +0700

    update code

--- js.js
+++ js.js
@@ -33,51 +33,123 @@ function register() {
     console.log(x);
     x.style.left = "-450px";
     y.style.left = "50px";
-    z.style.left = "0";
+    z.style.left = "110px";
 }
+
 
 function validate() {
-    var regex_username = /^[a-zA-Z]*$/g;
-    var regex_pw = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/g;
-    var regex_mail = /^[a-z0-9](\.?[a-z0-9])*@[a-z]{3,}.[a-z]{1,3}$/g;
-    var regex_phone = /^([0-9]{10,}$)/g;
     var err = document.getElementsByClassName("err");
     var check = true;
-    if (!regex_username.test(document.getElementById("user").value)) {
-        err[0].innerHTML = "Không hợp lệ"
-        check = false;
-    } else {
+    const fieldRequired = ["user", "Password", "Re_Password", "Email", "Phone"];
+    var rules;
+    var i = 0;
+    var re_pw = document.getElementById("Re_Password").value.trim();
+    fieldRequired.forEach(field => {
+        let value = document.getElementById(`${field}`).value.trim();
+        err[0].innerHTML = isUserName(value);
+        err[1].innerHTML = isPassword(value);
+        err[2].innerHTML = isRe_pw(value);
+        err[3].innerHTML = isMail(value);
+        err[4].innerHTML = isPhone(value);
+        (value);
 
-        err[0].innerHTML = "";
-    }
-    if (!regex_pw.test(document.getElementById("Password").value)) {
-        err[1].innerHTML = "Không hợp lệ"
-        check = false;
-
-    } else {
-        err[1].innerHTML = "";
-    }
-    if (document.getElementById("Password").value != document.getElementById("Re_Password").value) {
-        err[2].innerHTML = "Không hợp lệ"
-        check = false;
-    } else
-        err[1].innerHTML = "";
-    if (!regex_mail.test(document.getElementById("Email").value)) {
-        err[3].innerHTML = "Không hợp lệ"
-        check = false;
-
-    } else {
-        err[3].innerHTML = "";
-    }
-    if (!regex_phone.test(document.getElementById("Phone").value))
-
-    {
-        err[4].innerHTML = "Không hợp lệ"
-        check = false;
-
-    } else {
-        err[4].innerHTML = "";
-    }
-    if (check == true)
-        window.alert("Đăng kí thành công");
+    });
 }
+
+function required(value) {
+    return value
+}
+
+function isPassword(value) {
+    if (required(value)) {
+        var regex_pw = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/g;
+        if (!regex_pw.test(document.getElementById("Password").value)) {
+            return "Không hợp lệ"
+        } else
+            return "";
+    } else
+        return "Không được bỏ trống";
+
+
+}
+
+function isUserName(value) {
+    if (required(value)) {
+        var regex_username = /^[a-zA-Z]*$/g;
+        if (!regex_username.test(document.getElementById("user").value)) {
+            return "Không hợp lệ";
+        } else
+            return "";
+    } else
+        return "Không được bỏ trống";
+
+}
+
+function isMail(value) {
+    if (required(value)) {
+        var regex_mail = /^[a-z0-9](\.?[a-z0-9])*@[a-z]{3,}.[a-z]{1,3}$/g;
+    if (!regex_mail.test(document.getElementById("Email").value)) {
+            return "Không hợp lệ"
+        } else
+            return "";
+    } else
+        return "Không được bỏ trống";
+
+}
+
+function isPhone(value) {
+    if (required(value)) {
+        var regex_phone = /^([0-9]{10,}$)/g;
+    if (!regex_phone.test(document.getElementById("Phone").value)) {
+            return "Không hợp lệ"
+        } else
+            return "";
+    } else
+        return "Không được bỏ trống";
+
+}
+function isRe_pw(value)
+{
+    if(required(value))
+    {
+        var pw = document.getElementById("Password").value.trim();
+        if(pw!=value)
+            return "Password không khớp";
+        else
+            return "";
+    }
+    else
+        return "không được bỏ trống";
+}
+// switch (fieldRequired) {
+//     case "user":
+//         if (!isUserName(value)) {
+//             err[i].innerHTML = "Không hợp lệ";
+//         } else
+//             err[i].innerHTML = "";
+//         break;
+//     case "Password":
+//         if (!isUserName(value)) {
+//             err[i].innerHTML = "Không hợp lệ";
+//         } else
+//             err[i].innerHTML = "";
+//         break;
+//     case "Re_Password":
+//         if (value!=re_pw) {
+//             err[i].innerHTML = "Không hợp lệ";
+//         } else
+//             err[i].innerHTML = "";
+//         break;
+//     case "Email":
+//         if (!isUserName(value)) {
+//             err[i].innerHTML = "Không hợp lệ";
+//         } else
+//             err[i].innerHTML = "";
+//         break;
+//     case "Phone":
+//         if (!isUserName(value)) {
+//             err[i].innerHTML = "Không hợp lệ";
+//         } else
+//             err[i].innerHTML = "";
+//         break;
+{}
